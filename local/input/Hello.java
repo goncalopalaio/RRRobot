7aecc44921e3b80865d97bb7a8e4f5af9857593d
goncalopalaio <goncalopalaio@gmail.com>
2019-01-09 00:30:28 +0000

    Clean Hello

--- local/input/Hello.java
+++ local/input/Hello.java
@@ -1,11 +1,9 @@
-//import android.util.Log;
 package com.gplio.rrrobot.common;
 
 public class Hello implements IRRRobot {
 	
 	@Override
     public String run() {
-        //Log.d("RRRobot.MainActivity", "HELLLOOOOO");
         return "World";
     }
 }
