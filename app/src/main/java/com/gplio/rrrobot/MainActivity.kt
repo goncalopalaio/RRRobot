2f0a41a0eac0d834f05247696b91e0e71cdb98e2
goncalopalaio <goncalopalaio@gmail.com>
2019-01-12 17:07:19 +0000

    Request required permissions

--- app/src/main/java/com/gplio/rrrobot/MainActivity.kt
+++ app/src/main/java/com/gplio/rrrobot/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gplio.rrrobot
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -12,7 +13,10 @@ import dalvik.system.DexClassLoader
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import android.content.IntentFilter
-
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         val filter = IntentFilter()
         filter.addAction(EVENT_RECEIVER_ACTION)
         registerReceiver(eventReceiver, filter)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
     override fun onDestroy() {
@@ -60,13 +75,13 @@ class MainActivity : AppCompatActivity() {
         tv_body.text = body
     }
 
-    fun read(dex : File, cls : String): IRRRobot? {
+    fun read(dex: File, cls: String): IRRRobot? {
 
         try {
             val cacheDirectory = getDir("rrrobot-cache-outdex", Context.MODE_PRIVATE)
             Log.d(TAG, "CacheDirectory: $cacheDirectory")
 
-            val classLoader = DexClassLoader(dex.absolutePath, cacheDirectory.absolutePath,null, classLoader)
+            val classLoader = DexClassLoader(dex.absolutePath, cacheDirectory.absolutePath, null, classLoader)
 
             val moduleClass = classLoader.loadClass(cls)
             return moduleClass.newInstance() as IRRRobot
@@ -77,8 +92,19 @@ class MainActivity : AppCompatActivity() {
         return null
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onRequestPermissionsResult:: Permissions not granted")
+            Toast.makeText(this, "Without permissions the application won't function properly", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     companion object {
         const val TAG = "RRRobot.MainActivity"
         const val EVENT_RECEIVER_ACTION = "$TAG.EVENT_RECEIVER_ACTION"
+        const val PERMISSION_REQUEST_CODE = 1666
     }
 }
